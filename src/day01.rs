10901ae8cecd937d16976a283a76802ded244213
Connor Glosser <glosser1@gmail.com>
2019-12-01 16:29:05 -0500

    Use iterators instead of recursion
    This is, perhaps, more difficult to read, but it consolidates the "fuel calculation" to one location and explicitly terminates the sequence with a None type (instead of just zero or infinite zeros)

--- src/day01.rs
+++ src/day01.rs
@@ -4,32 +4,43 @@ use crate::utils;
 
 const INPUT: &str = "inputs/day01_input.txt";
 
-pub fn fuel(weight: &i64) -> i64 {
-    std::cmp::max(0, weight / 3 - 2)
+struct FuelSequence {
+    curr: i64,
+    next: i64,
 }
 
-fn total_fuel(weight: &i64) -> i64 {
-    fn helper(total: i64, weight: i64) -> i64 {
-        let f = fuel(&weight);
-        match f {
-            0 => total,
-            _ => helper(total + f, f),
+impl FuelSequence {
+    fn new(x: i64) -> FuelSequence {
+        FuelSequence { curr: 0, next: x }
+    }
+}
+
+impl Iterator for FuelSequence {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<i64> {
+        self.curr = self.next;
+        self.next = std::cmp::max(0, &self.curr / 3 - 2);
+
+        match self.curr {
+            0 => None,
+            _ => Some(self.curr),
         }
     }
+}
 
-    helper(0, weight.clone())
+fn eval(identifier: &str, f: fn(&i64) -> i64) {
+    let numbers = utils::read(File::open(INPUT).unwrap()).unwrap();
+    let result: i64 = numbers.iter().map(f).sum();
+    println!("{}: {}", identifier, result);
 }
 
 pub fn part1() {
-    let numbers = utils::read(File::open(INPUT).unwrap()).unwrap();
-    let result: i64 = numbers.iter().map(fuel).sum();
-    println!("Day 01a: {}", result);
+    eval("Day 01a", |x| FuelSequence::new(*x).nth(1).unwrap_or(0));
 }
 
 pub fn part2() {
-    let numbers = utils::read(File::open(INPUT).unwrap()).unwrap();
-    let result: i64 = numbers.iter().map(total_fuel).sum();
-    println!("Day 01b: {}", result);
+    eval("Day 01b", |x| FuelSequence::new(*x).skip(1).sum::<i64>());
 }
 
 #[cfg(test)]
@@ -38,17 +49,17 @@ mod test {
 
     #[test]
     fn fuel_examples() {
-        assert_eq!(0, fuel(&0));
-        assert_eq!(2, fuel(&12));
-        assert_eq!(2, fuel(&14));
-        assert_eq!(654, fuel(&1969));
-        assert_eq!(33583, fuel(&100756));
+        assert_eq!(None, FuelSequence::new(0).nth(1));
+        assert_eq!(Some(2), FuelSequence::new(12).nth(1));
+        assert_eq!(Some(2), FuelSequence::new(14).nth(1));
+        assert_eq!(Some(654), FuelSequence::new(1969).nth(1));
+        assert_eq!(Some(33_583), FuelSequence::new(100_756).nth(1));
     }
 
     #[test]
     fn total_fuel_examples() {
-        assert_eq!(2, total_fuel(&14));
-        assert_eq!(966, total_fuel(&1969));
-        assert_eq!(50346, total_fuel(&100756));
+        assert_eq!(2, FuelSequence::new(14).skip(1).sum::<i64>());
+        assert_eq!(966, FuelSequence::new(1969).skip(1).sum::<i64>());
+        assert_eq!(50346, FuelSequence::new(100756).skip(1).sum::<i64>());
     }
 }
