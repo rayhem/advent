dca55e35903ad8122a7361d5acdfd40f01f97bfc
Connor Glosser <glosser1@gmail.com>
2019-12-05 22:34:43 -0500

    Complete day 04

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod day01;
 mod day02;
+mod day04;
+mod problem;
 mod utils;
+
+use problem::Problem;
 
 fn main() {
     day01::part1();
@@ -8,4 +12,8 @@ fn main() {
 
     day02::part1();
     day02::part2();
+
+    let day04 = day04::Day04 {};
+    println!("Day 04a: {}", day04.part_one("146810-612564"));
+    println!("Day 04b: {}", day04.part_two("146810-612564"));
 }
