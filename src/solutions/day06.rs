9f84a32505fa4baf1eafd630cc6bc8d56a0a6e59
Connor Glosser <glosser1@gmail.com>
2019-12-27 17:34:07 -0500

    Solve part 2

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -9,23 +9,24 @@ impl Solution for Day06 {
     }
 
     fn part_two(&self, input: &str) -> String {
-        String::from("Hello from part_two")
+        let dist = System::from_str(input).distance(&"YOU", &"SAN") - 2;
+        dist.to_string()
     }
 }
 
-struct System {
-    orbits: HashMap<String, String>,
+struct System<'a> {
+    orbits: HashMap<&'a str, &'a str>,
 }
 
-impl System {
-    fn from_str(input: &str) -> Self {
+impl<'a> System<'a> {
+    fn from_str(input: &'a str) -> Self {
         System {
             orbits: input
                 .lines()
                 .map(|line| {
                     let mut t = line.split(')');
-                    let parent = t.next().unwrap().to_string();
-                    let child = t.next().unwrap().to_string();
+                    let parent = t.next().unwrap();
+                    let child = t.next().unwrap();
                     (child, parent)
                 })
                 .collect::<HashMap<_, _>>(),
@@ -44,22 +45,50 @@ impl System {
 
         count
     }
+
+    fn parents(&self, orbit: &'a str) -> Vec<&'a str> {
+        let mut parents: Vec<&'a str> = Vec::new();
+
+        let mut orbit = Some(orbit);
+        while let Some(o) = orbit {
+            parents.push(o);
+            orbit = self.orbits.get(o).map(|v| *v);
+        }
+
+        parents
+    }
+
+    fn distance(&self, a: &str, b: &str) -> usize {
+        let a_parents = self.parents(a);
+        let b_parents = self.parents(b);
+
+        let mut a_dist = 0;
+        let mut b_dist = 0;
+
+        for (i, a_parent) in a_parents.iter().enumerate() {
+            if let Some(j) = b_parents.iter().position(|b_parent| b_parent == a_parent) {
+                a_dist = i;
+                b_dist = j;
+                break;
+            }
+        }
+
+        a_dist + b_dist
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    const EXAMPLE: &str = "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L";
 
     #[test]
     fn part_one_example() {
-        let s = "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L";
-        assert_eq!(42, System::from_str(&s).num_orbits());
+        assert_eq!(42, System::from_str(EXAMPLE).num_orbits());
     }
 
     #[test]
     fn part_two() {
-        let s = "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L";
-
-        assert!(false);
+        assert_eq!(4, System::from_str(EXAMPLE).distance(&"K", &"I"));
     }
 }
