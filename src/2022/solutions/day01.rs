089e460cf7507f2034e4d8af1b791c21c4697eb8
Connor Glosser <glosser1@gmail.com>
2022-12-01 16:21:17 -0500

    Groundwork for 2022

---
+++ src/2022/solutions/day01.rs
@@ -0,0 +1,24 @@
+use utils::solution::Solution;
+
+pub struct Day01 {}
+
+impl Solution for Day01 {
+    fn part_one(&self, input: &str) -> Option<String> {
+        todo!();
+    }
+
+    fn part_two(&self, input: &str) -> Option<String> {
+        todo!();
+    }
+}
+
+#[cfg(test)]
+mod test {
+    mod unit {
+        todo!();
+    }
+
+    mod integration {
+        todo!();
+    }
+}
